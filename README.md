151e4432058fe07f9329542381d9998b563deff4
Nassim <achnassim34@gmail.com>
2024-05-18 00:10:42 +0200

    Optimisation et debuggage du code Javascript

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 Cette page offre la possiblité de sélectionner des liens vers des pages d'offre automobile selon des filtres proposés.
 
 # A faire :
-1. Optimiser le JS en regroupant les filtre en une boucle for
-2. Debugger les liens des cartes qui mène à un HTML sans CSS
+1. Debugger les liens des cartes qui mène à un HTML sans CSS
--- assets/script.js
+++ assets/script.js
@@ -44,56 +44,25 @@ function generationCartes() {
 
 generationCartes();
 
-//Définition des bouttons et appel des cartes selon les bouttons pressé
+//Définition des bouttons
 
 let btnLuxe = document.getElementById("btn-luxe");
 let btnSport = document.getElementById("btn-sport");
 let btnVintage = document.getElementById("btn-vintage");
 let btnAll = document.getElementById("btn-all");
 
-btnLuxe.addEventListener("click", (event) => {
-    // Effacer le contenu de la balise cartes
-    cartes.innerHTML = "";
+let listeBtn = [btnLuxe,btnSport,btnVintage];
 
-    // Parcourir les filtres et ajouter les nouvelles cartes
-            let lienBoite = document.createElement("a");
-            lienBoite.href = filtres[0].lien;
-            lienBoite.classList.add("boite-lien");
+//Création des bouttons selon le filtre pressé
 
-            let boite = document.createElement("div");
-            boite.classList.add("box-carte");
-            lienBoite.appendChild(boite);
+for (let i = 0; i < listeBtn.length; i++) {
+    listeBtn[i].addEventListener("click", () => {
+        // Effacer le contenu de la balise cartes
+        cartes.innerHTML = "";
 
-            let image = document.createElement("img")
-            image.src = filtres[0].image;
-            image.classList.add("image-voiture")
-            boite.appendChild(image)
-
-            let titre = document.createElement("h2");
-            titre.innerText = filtres[0].nom;
-            titre.classList.add("texte-balise")
-            boite.appendChild(titre);
-
-            let genre = document.createElement("h3");
-            genre.innerText = filtres[0].genre;
-            genre.classList.add("texte-balise")
-            boite.appendChild(genre);
-
-            let prix = document.createElement("h2");
-            prix.innerText = filtres[0].prix + "€";
-            prix.classList.add("texte-balise")
-            boite.appendChild(prix);
-
-            cartes.appendChild(lienBoite);
-});
-
-btnSport.addEventListener("click", (event) => {
-    // Effacer le contenu de la balise cartes
-    cartes.innerHTML = "";
-
-    // Parcourir les filtres et ajouter les nouvelles cartes
+        // Parcourir les filtres et ajouter les nouvelles cartes
         let lienBoite = document.createElement("a");
-        lienBoite.href = filtres[1].lien;
+        lienBoite.href = filtres[i].lien;
         lienBoite.classList.add("boite-lien");
 
         let boite = document.createElement("div");
@@ -101,63 +70,30 @@ btnSport.addEventListener("click", (event) => {
         lienBoite.appendChild(boite);
 
         let image = document.createElement("img")
-        image.src = filtres[1].image;
+        image.src = filtres[i].image;
         image.classList.add("image-voiture")
         boite.appendChild(image)
 
         let titre = document.createElement("h2");
-        titre.innerText = filtres[1].nom;
+        titre.innerText = filtres[i].nom;
         titre.classList.add("texte-balise")
         boite.appendChild(titre);
 
         let genre = document.createElement("h3");
-        genre.innerText = filtres[1].genre;
+        genre.innerText = filtres[i].genre;
         genre.classList.add("texte-balise")
         boite.appendChild(genre);
 
         let prix = document.createElement("h2");
-        prix.innerText = filtres[1].prix + "€";
+        prix.innerText = filtres[i].prix + "€";
         prix.classList.add("texte-balise")
         boite.appendChild(prix);
 
         cartes.appendChild(lienBoite);
-});
+    });
+}
 
-btnVintage.addEventListener("click", (event) => {
-    // Effacer le contenu de la balise cartes
-    cartes.innerHTML = "";
-
-    // Parcourir les filtres et ajouter les nouvelles cartes
-        let lienBoite = document.createElement("a");
-        lienBoite.href = filtres[2].lien;
-        lienBoite.classList.add("boite-lien");
-
-        let boite = document.createElement("div");
-        boite.classList.add("box-carte");
-        lienBoite.appendChild(boite);
-
-        let image = document.createElement("img")
-        image.src = filtres[2].image;
-        image.classList.add("image-voiture")
-        boite.appendChild(image)
-
-        let titre = document.createElement("h2");
-        titre.innerText = filtres[2].nom;
-        titre.classList.add("texte-balise")
-        boite.appendChild(titre);
-
-        let genre = document.createElement("h3");
-        genre.innerText = filtres[2].genre;
-        genre.classList.add("texte-balise")
-        boite.appendChild(genre);
-
-        let prix = document.createElement("h2");
-        prix.innerText = filtres[2].prix + "€";
-        prix.classList.add("texte-balise")
-        boite.appendChild(prix);
-
-        cartes.appendChild(lienBoite);
-});
+//Exception pour le filtre btnAll qui se créér en appelant la fonction originel de la génération de cartes
 
 btnAll.addEventListener("click", (event) => {
     cartes.innerHTML = "";
